3533882122f65103db750d7b342b111c869befaf
Zack <zack@ukaton.com>
2024-01-22 13:03:53 -0800

    fixes

--- examples/ar-session/body-tracking/script.js
+++ examples/ar-session/body-tracking/script.js
@@ -95,9 +95,64 @@ ARSessionManager.addEventListener("showCamera", (event) => {
     toggleShowCameraButton.innerText = showCamera ? "hide camera" : "show camera";
 });
 
+/** @typedef {import("../../src/three/three.module.min.js").Vector3} Vector3 */
+/** @typedef {import("../../src/three/three.module.min.js").Euler} Euler */
+/** @typedef {import("../../src/three/three.module.min.js").Quaternion} Quaternion */
+/** @typedef {import("../../src/three/three.module.min.js").PerspectiveCamera} PerspectiveCamera */
+
+const aframeCamera = document.getElementById("camera");
+/** @type {PerspectiveCamera} */
+var threeCamera;
+const cameraPositionSpan = document.getElementById("cameraPosition");
+
+/** @type {Vector3} */
+const cameraPosition = new THREE.Vector3();
+/** @type {Quaternion} */
+const cameraQuaternion = new THREE.Quaternion();
+
+var latestFocalLength;
+ARSessionManager.addEventListener("camera", (event) => {
+    /** @type {import("../../../src/ARSessionManager.js").ARSCamera} */
+    const camera = event.message.camera;
+    //console.log("camera data", camera);
+
+    cameraPosition.set(...camera.position);
+    cameraQuaternion.set(...camera.quaternion);
+
+    aframeCamera.object3D.position.copy(cameraPosition);
+    aframeCamera.object3D.quaternion.copy(cameraQuaternion);
+
+    cameraPositionSpan.innerText = cameraPosition
+        .toArray()
+        .map((v) => v.toFixed(3))
+        .join(",");
+
+    threeCamera = threeCamera || aframeCamera?.components?.camera?.camera;
+    if (threeCamera) {
+        if (latestFocalLength != camera.focalLength) {
+            threeCamera.setFocalLength(camera.focalLength * 1.14);
+            latestFocalLength = camera.focalLength;
+        }
+    }
+
+    scene.renderer.toneMappingExposure = camera.exposureOffset;
+});
+
+const sky = document.querySelector("a-sky");
+ARSessionManager.addEventListener("showCamera", (event) => {
+    /** @type {boolean} */
+    const showCamera = event.message.showCamera;
+    console.log("showCamera", showCamera);
+    const newVisible = !showCamera;
+    if (newVisible != sky.object3D.visible) {
+        sky.object3D.visible = newVisible;
+    }
+});
+
 const bodyEntityBones = {};
+window.bodyEntityBones = bodyEntityBones;
 const bodyEntity = document.getElementById("body");
-bodyEntity.addEventListener("model-loaded", (event) => {
+bodyEntity.addEventListener("model-loaded", () => {
     const model = bodyEntity.components["gltf-model"].model;
     model.traverse((object) => {
         if (object.type == "Bone") {
@@ -108,6 +163,16 @@ bodyEntity.addEventListener("model-loaded", (event) => {
     console.log(bodyEntityBones);
 });
 
+/** @type {Vector3} */
+const _position = new THREE.Vector3();
+/** @type {Vector3} */
+const _scale = new THREE.Vector3();
+/** @type {Quaternion} */
+const _quaternion = new THREE.Quaternion();
+
+const bodyPositionSpan = document.getElementById("bodyPosition");
+var latestEstimatedScaleFactor;
+
 /** @typedef {import("../../../src/ARSessionManager.js").ARSBodyAnchor} ARSBodyAnchor */
 ARSessionManager.addEventListener("bodyAnchors", (event) => {
     /** @type {ARSBodyAnchor[]} */
@@ -115,7 +180,36 @@ ARSessionManager.addEventListener("bodyAnchors", (event) => {
 
     const bodyAnchor = bodyAnchors[0];
     if (bodyAnchor) {
-        console.log("bodyAnchor", bodyAnchor);
-        var x = bodyAnchor.skeleton["hey"];
+        //console.log("bodyAnchor", bodyAnchor);
+
+        _position.set(...bodyAnchor.position);
+        _quaternion.set(...bodyAnchor.quaternion);
+
+        bodyEntity.object3D.position.lerp(_position, 0.5);
+        bodyEntity.object3D.quaternion.slerp(_quaternion, 0.5);
+
+        if (latestEstimatedScaleFactor != bodyAnchor.estimatedScaleFactor) {
+            _scale.setScalar(bodyAnchor.estimatedScaleFactor);
+            bodyEntity.object3D.scale.copy(_scale);
+            latestEstimatedScaleFactor = bodyAnchor.estimatedScaleFactor;
+        }
+
+        bodyPositionSpan.innerText = _position
+            .toArray()
+            .map((v) => v.toFixed(3))
+            .join(",");
+
+        Object.entries(bodyAnchor.skeleton).forEach(([boneName, joint]) => {
+            const bodyEntityBone = bodyEntityBones[boneName];
+            if (bodyEntityBone) {
+                _position.set(...joint.position);
+                _quaternion.set(...joint.quaternion);
+
+                //bodyEntityBone.position.lerp(_position, 0.5);
+                bodyEntityBone.quaternion.slerp(_quaternion, 0.5);
+            } else {
+                console.error(`"${boneName}" bone not found in entity`);
+            }
+        });
     }
 });
--- src/ARSessionManager.js
+++ src/ARSessionManager.js
@@ -185,6 +185,8 @@ const _console = createConsole("ARSession", { log: false });
  * @typedef ARSBodyAnchor
  * @type {object}
  * @property {string} identifier
+ * @property {boolean} isTracked
+ * @property {number} estimatedScaleFactor
  * @property {number[]} position
  * @property {number[]} quaternion
  * @property {ARSSkeleton} skeleton
@@ -193,7 +195,7 @@ const _console = createConsole("ARSession", { log: false });
 /** @typedef {"root" | "hips_joint" | "left_upLeg_joint" | "left_leg_joint" | "left_foot_joint" | "left_toes_joint" | "left_toesEnd_joint" | "right_upLeg_joint" | "right_leg_joint" | "right_foot_joint" | "right_toes_joint" | "right_toesEnd_joint" | "spine_1_joint" | "spine_2_joint" | "spine_3_joint" | "spine_4_joint" | "spine_5_joint" | "spine_6_joint" | "spine_7_joint" | "neck_1_joint" | "neck_2_joint" | "neck_3_joint" | "neck_4_joint" | "head_joint" | "jaw_joint" | "chin_joint" | "nose_joint" | "right_eye_joint" | "right_eyeUpperLid_joint" | "right_eyeLowerLid_joint" | "right_eyeball_joint" | "left_eye_joint" | "left_eyeUpperLid_joint" | "left_eyeLowerLid_joint" | "left_eyeball_joint" | "right_shoulder_1_joint" | "right_arm_joint" | "right_forearm_joint" | "right_hand_joint" | "right_handPinkyStart_joint" | "right_handPinky_1_joint" | "right_handPinky_2_joint" | "right_handPinky_3_joint" | "right_handPinkyEnd_joint" | "right_handRingStart_joint" | "right_handRing_1_joint" | "right_handRing_2_joint" | "right_handRing_3_joint" | "right_handRingEnd_joint" | "right_handMidStart_joint" | "right_handMid_1_joint" | "right_handMid_2_joint" | "right_handMid_3_joint" | "right_handMidEnd_joint" | "right_handIndexStart_joint" | "right_handIndex_1_joint" | "right_handIndex_2_joint" | "right_handIndex_3_joint" | "right_handIndexEnd_joint" | "right_handThumbStart_joint" | "right_handThumb_1_joint" | "right_handThumb_2_joint" | "right_handThumbEnd_joint" | "left_shoulder_1_joint" | "left_arm_joint" | "left_forearm_joint" | "left_hand_joint" | "left_handPinkyStart_joint" | "left_handPinky_1_joint" | "left_handPinky_2_joint" | "left_handPinky_3_joint" | "left_handPinkyEnd_joint" | "left_handRingStart_joint" | "left_handRing_1_joint" | "left_handRing_2_joint" | "left_handRing_3_joint" | "left_handRingEnd_joint" | "left_handMidStart_joint" | "left_handMid_1_joint" | "left_handMid_2_joint" | "left_handMid_3_joint" | "left_handMidEnd_joint" | "left_handIndexStart_joint" | "left_handIndex_1_joint" | "left_handIndex_2_joint" | "left_handIndex_3_joint" | "left_handIndexEnd_joint" | "left_handThumbStart_joint" | "left_handThumb_1_joint" | "left_handThumb_2_joint" | "left_handThumbEnd_joint"} ARSSkeletonJointName */
 /**
  * @typedef ARSSkeleton
- * @type {object.<string, ARSSkeletonJoint>}
+ * @type {Object.<string, ARSSkeletonJoint>}
  */
 
 /**
